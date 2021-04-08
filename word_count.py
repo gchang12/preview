2741ea49618236fff0e7416d6dd61105aa53e346
George Chang <59218296+gchang12@users.noreply.github.com>
2021-04-08 10:06:47 -1000

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -25,5 +25,7 @@ def word_count_calculator(scene_folder,stop_scene=0):
     return total
 
 if __name__ == '__main__':
+    scene_folder=sep.join(['.','scenes'])
+    wc=word_count_calculator(scene_folder,stop_scene=5)
     message='\nThe word count is: %d.'%wc
     print(message)
