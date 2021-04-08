7fe43b6c916c59c1c0c8623f51bece8b17961309
George Chang <59218296+gchang12@users.noreply.github.com>
2021-04-08 09:55:44 -1000

    Add files via upload

--- tex-story.bat
+++ tex-story.bat
@@ -1,18 +1,16 @@
 @echo off
-set scenedir=.\scenes
-set targetdir=..\scenes
-set sourcedir=.\raw
-set texdir=.\tex
-set errmsg=echo Please redownload the raw story files from my GitHub and reinsert them into the root of this directory: https://github.com/gchang12/preview
-if not exist %sourcedir% %errmsg% & exit /b
-if not exist %texdir% %errmsg% & exit /b
-if not exist %scenedir% md %scenedir%
+set homedir=%cd%
+set targetdir=C:\Users\Eclair\Documents\projects\latex\preview\scenes
+if not exist %targetdir% md %targetdir%
+set sourcedir=C:\Users\Eclair\Documents\projects\untitled
 cd %sourcedir%
 forfiles /p . /c "cmd /c if @isdir == "FALSE" copy @file %targetdir%"
+cd %targetdir%
+py C:\Users\Eclair\Documents\projects\python\overused\quotes.py
 cd ..
-py quotes.py
-cd %texdir%
 pdflatex preview
-start preview.pdf
-cd ..
-py word_count.py
+set story=C:\Users\Eclair\Documents\projects\latex\preview\preview.pdf
+start %story%
+py C:\Users\Eclair\Documents\projects\python\overused\word_count.py
+if not exist D:\ exit /b
+copy /y %story% D:\NOOK\
