8a08f1d30630ee72e89d45a9d54c06e6d7f72748
George Chang <59218296+gchang12@users.noreply.github.com>
2021-04-19 08:45:41 -1000

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -55,7 +55,7 @@ def fix_file_quote(file):
             wfile.write(line)
 
 def fix_prose():
-    story_folder=sep.join(['.','scenes'])
+    story_folder=sep.join(['.','raw'])
     for dirpath,dirnames,filenames in walk(story_folder):
         if dirpath != story_folder:
             continue
